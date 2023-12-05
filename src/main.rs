49c74a2be8b936ca34166fb323fd967d362d12ea
Nathan Strong <gblues@gmail.com>
2023-12-04 22:40:46 -0800

    part 2 solution

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,32 @@ fn main() {
     let input = include_str!("input/4.txt");
 
     let card_pattern = Regex::new(r"(?m)^Card\s+(\d+):\s+([^|]+)\s+\|\s+([^|]+)\s*$").unwrap();
-    let mut total: u32 = 0;
+    let total: u32;
 
-    for (_, [card_id, winning_numbers, have_numbers]) in card_pattern.captures_iter(input).map(|c|c.extract::<3>()) {
-        let winning_set: HashSet<i32> = HashSet::from_iter(winning_numbers.split_whitespace().map(|n| n.parse::<i32>().unwrap()).collect::<Vec<_>>());
-        let have_set: HashSet<i32> = HashSet::from_iter(have_numbers.split_whitespace().map(|n| n.parse::<i32>().unwrap()).collect::<Vec<_>>());
-        let count: u32 = winning_set.intersection(&have_set).count() as u32;
-        if count > 0 {
-            let points:u32 = 2_i32.pow(count - 1) as u32;
-            println!("card {card_id} has {count} matches worth {points} points");
-            total += points;
+    let matches: Vec<u32> = card_pattern.captures_iter(input)
+        .map(|c|c.extract::<3>())
+        .map(|(_, [card_id, winning_numbers, have_numbers])| {
+            let winning_set: HashSet<i32> = HashSet::from_iter(
+                winning_numbers.split_whitespace().map(|n| n.parse::<i32>().unwrap()).collect::<Vec<_>>()
+            );
+            let have_set: HashSet<i32> = HashSet::from_iter(
+                have_numbers.split_whitespace().map(|n| n.parse::<i32>().unwrap()).collect::<Vec<_>>()
+            );
+
+            let count: u32 = winning_set.intersection(&have_set).count() as u32;
+            println!("Card {card_id} has {count} matches");
+            return count;
+        }).collect::<Vec<_>>();
+    let mut card_counts: Vec<u32> = vec![1; matches.len().try_into().unwrap()];
+    for i in 0..matches.len() {
+        if matches[i] > 0 {
+            for _ in 1..= card_counts[i] as usize {
+                for card in 1..=matches[i] as usize {
+                    card_counts[i + card] += 1;
+                }
+            }
         }
     }
-    println!("Total points: {total}");
+    total = card_counts.iter().sum();
+    println!("Total cards: {}", total);
 }
